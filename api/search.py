79dccb698ec612ae91b79a67a84eddf54dd05df0
Jad Assaf <jad.a.assaf@gmail.com>
2025-03-03 13:24:21 +0200

    test

--- api/search.py
+++ api/search.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # <-- Import Flask-Cors
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://macarabia.me"}})
 
 DB_URI = os.environ.get("DB_URI")
 
